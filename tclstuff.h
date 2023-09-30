db4c1c87199c0dd79c51d5a0a96a601ce5d1a210
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2023-09-30 10:18:46 +0200

    Add CHECK_RANGE_ARGS_LABEL

--- tclstuff.h
+++ tclstuff.h
@@ -71,6 +71,15 @@
 		} \
 	} while(0);
 
+#define CHECK_RANGE_ARGS_LABEL(label, rc, msg) \
+	do { \
+		if (objc < A_args || objc > A_objc) { \
+			Tcl_WrongNumArgs(interp, A_cmd+1, objv, msg); \
+			rc = TCL_ERROR; \
+			goto label; \
+		} \
+	} while(0);
+
 
 // A rather frivolous macro that just enhances readability for a common case
 #define TEST_OK( cmd )		\
