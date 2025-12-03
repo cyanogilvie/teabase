a0d3206a1b6e47f0564028ab64acbf4ef23b0271
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2025-12-03 18:06:37 -0300

    meson TEA debugging

--- tools/make_termdebug_arg.tcl
+++ tools/make_termdebug_arg.tcl
@@ -1,10 +1,10 @@
-lassign $argv build_root tclsh package_name package_version
+lassign $argv build_dir source_root tclsh package_name package_version
 
 puts -nonewline {Termdebug -ex set\ print\ pretty\ on --args }
 puts -nonewline [string map {{ } {\ }} $tclsh]
-puts -nonewline { tests/all.tcl -load }
+puts -nonewline " [file join $source_root tests/all.tcl] -load "
 puts -nonewline [string map {{ } {\ }} {apply {{dir n v} {source [file join $dir pkgIndex.tcl]; package require -exact $n $v}} }]
-puts -nonewline [string map {{ } {\ }} [list $build_root $package_name $package_version]]
+puts -nonewline [string map {{ } {\ }} [list $build_dir $package_name $package_version]]
 puts -nonewline { }
 if {[info exists env(TESTFLAGS)]} {
 	puts -nonewline $env(TESTFLAGS)
