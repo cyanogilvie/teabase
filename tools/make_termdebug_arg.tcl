812a026e27fd016c80c0d6bb48d779737dd6e2e1
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2025-12-03 18:23:56 -0300

    Force -singleproc (or gdb breakpoints don't work)

--- tools/make_termdebug_arg.tcl
+++ tools/make_termdebug_arg.tcl
@@ -2,7 +2,7 @@ lassign $argv build_dir source_root tclsh package_name package_version
 
 puts -nonewline {Termdebug -ex set\ print\ pretty\ on --args }
 puts -nonewline [string map {{ } {\ }} $tclsh]
-puts -nonewline " [file join $source_root tests/all.tcl] -load "
+puts -nonewline " [file join $source_root tests/all.tcl] -singleproc 1 -load "
 puts -nonewline [string map {{ } {\ }} {apply {{dir n v} {source [file join $dir pkgIndex.tcl]; package require -exact $n $v}} }]
 puts -nonewline [string map {{ } {\ }} [list $build_dir $package_name $package_version]]
 puts -nonewline { }
