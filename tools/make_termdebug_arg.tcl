3c3c4e1aafd1889035732b523eb1882143979566
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2026-04-28 19:31:06 -0300

    Fix devenv, vim-gdb TESTFLAGS wiring

--- tools/make_termdebug_arg.tcl
+++ tools/make_termdebug_arg.tcl
@@ -4,9 +4,4 @@ puts -nonewline {Termdebug -ex set\ print\ pretty\ on --args }
 puts -nonewline [string map {{ } {\ }} $tclsh]
 puts -nonewline " [file join $source_root tests/all.tcl] -singleproc 1 -load "
 puts -nonewline [string map {{ } {\ }} {apply {{dir n v} {source [file join $dir pkgIndex.tcl]; package require -exact $n $v}} }]
-puts -nonewline [string map {{ } {\ }} [list $build_dir $package_name $package_version]]
-puts -nonewline { }
-if {[info exists env(TESTFLAGS)]} {
-	puts -nonewline $env(TESTFLAGS)
-}
-puts ""
+puts [string map {{ } {\ }} [list $build_dir $package_name $package_version]]
